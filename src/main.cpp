cb2b297822a016787ddb4c039a6ddcc0e3bd0ff6
jose <jabelcastellanosjoo@unm.edu>
2020-10-14 17:06:14 -0600

    Update README.md

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,11 @@
 #include "TexWriter.h"
+#include <iostream>
 
 int main(int argc, char * argv[]){
   if(argc == 2){
-    TexWriter what(argv[1]);
+    TexWriter quicktex(argv[1]);
     return 0;
   }
-
-  std::string project_name;
-  std::cin >> project_name;
-  TexWriter what(project_name);
-
-  return 0;
+  std::cerr << "Error: quicktex requires an argument denoting the name of the project." << std::endl;
+  return 1;
 }
